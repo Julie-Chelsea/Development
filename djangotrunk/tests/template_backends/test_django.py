330593c3dd152a55f35c09b8e2f5d7396d7d53c7
Chelsea Valentine <cv851@nyu.edu>
2015-01-05 00:03:18 -0500

    fix

--- djangotrunk/tests/template_backends/test_django.py
+++
@@ -1,9 +0,0 @@
-from django.template.backends.django import DjangoTemplates
-
-from .test_dummy import TemplateStringsTests
-
-
-class DjangoTemplatesTests(TemplateStringsTests):
-
-    engine_class = DjangoTemplates
-    backend_name = 'django'
