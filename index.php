6c57adc2b7727085b52939ad04be71479bf26e82
Chelesa Valentine <cv851@nyu.edu>
2015-03-15 01:40:22 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+        <title>Korean</title>
+        <link rel="stylesheet" type="text/css" href="layout/style.css">
+        <link rel="icon" href="img/favicon.png" type="image/png" sizes="16x16">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    </head>
+    <body class="confettied">
+    	<center>
+            <p>Hello 안녕하세요! 저는 첼시 발렌ㅊ타인이에요!whyㅇㅇㅇㅇ</p>
+    	
+        </center>
+        
+        <script src="layout/script.js" type="text/javascript"></script>
+    </body>
+</html>
